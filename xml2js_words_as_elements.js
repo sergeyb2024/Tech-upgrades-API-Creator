95411c84d211eb069f5eaaefb189e94a2d47b6a2
Sea-Moryak <164082172+sergeyb2024@users.noreply.github.com>
2024-05-06 15:44:22 -0400

    shorten code with Map, output keys as id's and values as description strings

--- xml2js_words_as_elements.js
+++ xml2js_words_as_elements.js
@@ -1,33 +1,37 @@
 const fs = require('fs'),
 xml2js = require('xml2js');
 const { log: C}  = require('console');
+const { type } = require('os');
 
-// start_3
+// start_1
 // --xml_words_as_elements
 
 
 const parser = new xml2js.Parser();
 fs.readFile('./MiamiGP_8_as_words.xml', function(err, data) {
     parser.parseString(data, function (err, result) {
+        const generalMap = new Map()
         for(const [key, value] of Object.entries(result)){
-            const vFlow = value.Flow
-            vFlow.forEach(item => {
-                for(const text of item.Para){
-                    const scrapedObject = {
-                        keyId: Object.values(item.$),
-                        textLine: text.Line,
-                        words: Object.values(text)[1]                       
+            generalMap.set(key, value)
+        }
+        for(const [key, values] of generalMap.entries()){
+            generalMap.set(key, values.Flow)
+        }
+        const myMap = new Map(generalMap)
+        const finalMap = new Map()
+        for(const [key, val] of myMap.entries()){
+            for(const x of val){
+                for(const para of x.Para){
+                    for(const line of para.Line){
+                        finalMap.set(para.$, line.Word)
+                        const finalObj = {
+                            key: para.$.id,
+                            value: line.Word.toString()
+                        }
+                        C(finalObj)
                     }
-                    const textObj = {
-                        key: scrapedObject.keyId,
-                        value: scrapedObject.textLine
-                    }
-                    const wordVal = textObj.value
-                    for(const [key, val] of Object.entries(wordVal)){
-                        C('key', key, 'keyId', scrapedObject.keyId, 'val',val.Word)
-                    }                    
                 }
-            })            
+            }
         }
     });
 });
