56f74f1d51ad78e7a4857e6715523fef91cb4b28
Sea-Moryak <164082172+sergeyb2024@users.noreply.github.com>
2024-06-15 09:44:41 -0400

    simplified code and now matching the right keys and values

--- xml2js.js
+++ xml2js.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const xml2js = require('xml2js');
 const { log: C } = require('console');
-const testingData = require('./mock-data')
+const testingData = require('./mock-data');
 
 
 /**
@@ -14,49 +14,97 @@ const testingData = require('./mock-data')
 
 
 
-/**
- *  checkRegex will respond with an object
- *  with key = "compornents" || "reasonForUpdate"
- *  it will serve to asign values to proper API keys
- */
+const testObj = {
+    Year: 2024,
+    RaceNo: "",
+    RaceName: "",
+    Constructor: "",
+    ComponentNo: "",
+    UpdatedComponent: "",
+    PrimaryReason: "",
+    GeometricDifferences: "",
+    Description: "",
+}
 
-const checkRegex = (stringData) => {
-    // api structure blueprint
-    const testObj = {
-          Year: Number,
-          RaceNo: Number,
-          RaceName: String,
-          Constructor: String,
-          ComponentNo: Number,
-          UpdatedComponent: String,
-          PrimaryReason: String,
-          GeometricDifferences: String,
-          Description: String,
+function getPrimaryReason (string) {
+    /**
+     * identical function as get Description. I will combine both late to reuse the code
+     */
+    for (const sentence of string.split('.')) {
+        const trimmedSentence = sentence.trim();
+        const validateSentenceLength = trimmedSentence && trimmedSentence[0].toUpperCase() === trimmedSentence[0] && trimmedSentence.split(' ').length
+
+        let lowercaseCount = 0;
+        let uppercaseCount = 0;
+        for (const word of trimmedSentence.split(' ')) {
+            if (word.toLowerCase() === word) {
+                lowercaseCount++;
+            } else {
+                uppercaseCount++;
+            }
+        }
+        
+        if(validateSentenceLength < 7 && uppercaseCount > 4  && trimmedSentence.indexOf(' As Well As')){
+           return testObj.PrimaryReason = trimmedSentence
+        }
     }
-    
-    const regexPattern = /(?<key>Year|RaceNo|RaceName|Constructor|ComponentNo|UpdatedComponent|PrimaryReason|GeometricDifferences|Description)[^a-zA-Z]+(?<value>[^,]+)/g;
-    
-    let match;
-    while ((match = regexPattern.exec(stringData)) !== null) {
-      const { key, value } = match.groups;
-      testObj[key] = value.trim();
-    }
-    return testObj;
 }
 
 
-/**
- *  obj keys used to build the object
- *  k1, k2, k3
-*/
-const k1 = 'id'; const k2 = 'description'; const k3 = 'output'
-var obj = {}; var paragraphs; var line;
+function inputComponents(string) {
+    /**
+     * This should recognise the component names and place into key value pairs
+     */
+    const regex = /|Nose|Sidepod Inlet|Floor Edge|Cooling - Louvres|Coke\/Engine - Cover|Front Wing Endplate|Floor Body|Beam Wing|Rear Wing|Rear Corner|Rear Suspension|Front Suspension|Floor Fences|Diffuser|Front Corner|Floor Corner|Halo|Headrest|Mirror|Front Wing/gm;
+    let m;
+    for(const words of string.split('.')){
+        if(words.indexOf(regex)){
+            testObj.UpdatedComponent = regex.exec(string)
+        }
+    }
+}
+
+
+function getDescriptionsString(string) {
+
+    /**
+     *  function will validate number of words as well as capital letter.
+     *  usually descriptions are the longest. Updated components have 2 to 3 words and Primary reason may vary,
+     *  therefore each value has an "as well as"
+     */
+    
+    for (const sentence of string.split('.')) {
+        const trimmedSentence = sentence.trim();
+        const validateSentenceLength = trimmedSentence && trimmedSentence[0].toUpperCase() === trimmedSentence[0] && trimmedSentence.split(' ').length
+
+        let lowercaseCount = 0;
+        let uppercaseCount = 0;
+        for (const word of trimmedSentence.split(' ')) {
+            if (word.toLowerCase() === word) {
+                lowercaseCount++;
+            } else {
+                uppercaseCount++;
+            }
+        }
+        if (validateSentenceLength >= 10) {
+            if (uppercaseCount < 12) {
+               return testObj.Description = trimmedSentence;
+            }
+        } else if(validateSentenceLength <= 2 && uppercaseCount <= 2){
+           return testObj.UpdatedComponent = trimmedSentence
+        }
+        
+    }
+
+}
+
+
+var paragraphs; var line;
 const parser = new xml2js.Parser();
 try {
 
     /**
-     * There are multiple PDF files that will need to be inside an array
-     * for FS to process as bulk and merge directly to update existing API
+     *  Read xml file page by page to extract text
      */
     fs.readFile('./MiamiGP-z-o.xml', 'utf-8', (err, data) => {
         err ?? C(err)
@@ -72,75 +120,30 @@ try {
             }
             paragraphs.map(element => {
                 line = element.Line
+                const newArr = []
+                newArr.push(line)
+                /**
+                 *  Format text to convinient format in order to fit the exisiting API structure
+                 */
+                const firstTrim = newArr.slice(-1).toString().split('\n').join(';').trim().replace(/,\s/, ' ')
+                const secondTrim = firstTrim.split('-,;').join('As Well As ')
+                const thirdTrim = secondTrim.split(/(?<=\w),;(?=[a-z]\w)/g).join(' ').replace('Front,;Suspension', 'Front Suspension')
+                // const fourthTrim = thirdTrim.split(/-?\W\;(?=[A-Z]\w|\s\[a-z]\w)/g).join('. ').replace(/(?=\;\d)/g, '--')
+                const fourthTrim = thirdTrim.split(/-?\W\;(?=[A-Z]\w|\s\[a-z]\w)/g).join('. ').replace('Coke/Engine. Cover', 'Coke/Engine Cover').replace('Cooling. Louvres', 'Cooling Louvres')
 
                 /**
-                 *  separates id's from all other string by
-                 *  validating if isNaN, will attach Number as ID
-                 *  and all the rest is nested in an object.
-                 *  Number of id's is the number of rows per 
-                 *  given sheet
+                 *  Call on each function to validate which text is appropriate for 
+                 *  the individual key/values
                  */
-                !isNaN(parseInt(element.Line)) ? 
-                obj[k1] = parseInt(line) : 
-                obj[k2] = {}; 
-                obj[k2][k3] = element.Line
-                C("obj.desc ",obj.description.output.toString())
+                testObj.PrimaryReason = getPrimaryReason(fourthTrim)
+                testObj.Description = getDescriptionsString(fourthTrim)
 
-                /**
-                 * tester for regex
-                 */
-                const testerForRegex = obj.description.output.toString()
-                for(let i = 0; i < testerForRegex; i++){
-                    if(testerForRegex[i] === Number){
-                        const newobj = {id: [i], desc : {
-                            details: testerForRegex[i] !== Number
-                        }} 
-                        C(newobj)
-
-                    }
-                }
-                const foundata = checkRegex('Front Corner')
-                C('found data ',foundata)
-              
             });
         });
     })
 }
 catch (err) {
     return `${err} while reading or parsing file`
-}
-
-/**
- *  writeFile is used to create a preliminary 
- *  JSON file for the API. Temporary testing
- *  solution.
- */
-function writeFile(data){
-    try{
-        fs.writeFileSync('testApi.json', JSON.stringify(data),  {
-            encoding: "utf-8"
-        }, () => {
-            C('successfully wrote to directory')
-        })
-    }catch(error){
-        return `${error} while writing file`
-    }
-}
-
-
-/**
- *  removes duplicates to get exact list
- *  of existing parts in order to create
- *  a regex filter. Use testData.js
- */
-function removeDuplicates(arr) {
-    let unique = arr.reduce(function (acc, curr) {
-            if (!acc.includes(curr.UpdatedComponent))
-                acc.push(curr.UpdatedComponent);
-            return acc;
-        }, []);
-        
-    return unique.toString();
 }
 
 
